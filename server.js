0a885d51e0901f34ec6bbb56dd76ae948bc01a82
PandhuWibowo <pandhuw58@gmail.com>
2020-07-13 21:55:09 +0700

    set up config is done

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('We are on home');
+require('dotenv/config');
+var mysql = require('mysql');
+
+app.use(bodyParser.json());
+
+var connection = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE,
+    multipleStatements : true,
 });
 
-app.listen(3000);
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connect');
+});
+
+app.listen(5000);
